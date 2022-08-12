4e440d39802a8adaf7f047b0dcdcf7a1f1a5d195
Matheus <matheus.augustoreal@gmail.com>
2022-08-12 13:29:49 -0300

    Getting dynamic information from GraphCMS

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,15 +1,65 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css'
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      id
+      description
+      videoId
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonsBySlugResponse {
+  lesson: {
+    title: string;
+    videoId: string;
+    description: string;
+    teacher: {
+      bio: string;
+      avatarURL: string;
+      name: string;
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonsBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  })
+
+  console.log(data)
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId="Exg8YBHB02s" />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -19,22 +69,22 @@ export function Video() {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
+              {data.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Conheça o Ignite Lab, um bônus exclusivo no qual você vai codar um projeto completo usando tecnologias modernas!
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 my-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/diego3g.png"
+                src={data.lesson.teacher.avatarURL}
                 alt=""
               />
 
               <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">Diego Fernandes</strong>
-                <strong className="text-gray-200 text-sm block">CTO @Rocketseat</strong>
+                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                <strong className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</strong>
               </div>
             </div>
           </div>
