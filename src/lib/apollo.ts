1a75bc4a798e4bb2b21221751fd46c3bfa500239
Matheus <matheus.augustoreal@gmail.com>
2022-08-13 10:56:09 -0300

    Created subscribe mutation

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.hygraph.com/v2/cl6p2qupo1hyf01t94vc0fid9/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 });
